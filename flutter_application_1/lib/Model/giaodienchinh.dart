a08a5d2d55420428569674183eb47cbdd17469aa
QuyetChienTran <tranquyetchien1708@gmail.com>
2022-11-12 21:45:00 +0700

    add giao dien chinh

---
+++ flutter_application_1/lib/Model/giaodienchinh.dart
@@ -0,0 +1,293 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(
+        title: '',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("images/nen.jpg"), fit: BoxFit.cover),
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(20),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 50, 10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              "images/star.png",
+                              fit: BoxFit.cover,
+                              width: 20,
+                            ),
+                            Container(
+                              padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 1, color: Colors.black),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: const Text('2'),
+                            ),
+                            Image.asset("images/add.png",
+                                fit: BoxFit.cover, width: 20)
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset("images/setting.png", width: 30),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Text(
+                    'Level 1',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.redAccent),
+                  ),
+                  Image.asset(
+                    "images/lion.png",
+                    fit: BoxFit.cover,
+                    width: 120,
+                  ),
+                  const Text(
+                    'Player #123456',
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.redAccent),
+                  ),
+                  const Padding(padding: EdgeInsets.all(20)),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                            const Color.fromARGB(255, 27, 247, 228)
+                                .withOpacity(0.8)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)))),
+                    onPressed: () => {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 3.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                'images/guom.png',
+                                width: 45,
+                              ),
+                            ],
+                          ),
+                          const Text(
+                            'BẮT ĐẦU',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(6)),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                            const Color.fromARGB(255, 27, 247, 228)
+                                .withOpacity(0.8)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)))),
+                    onPressed: () => {},
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(25, 10, 25, 10),
+                      child: Text(
+                        'LỊCH SỬ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(6)),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                            const Color.fromARGB(255, 27, 247, 228)
+                                .withOpacity(0.8)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)))),
+                    onPressed: () => {},
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Text(
+                        'ĐIỂM CAO',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(6)),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                            const Color.fromARGB(255, 27, 247, 228)
+                                .withOpacity(0.8)),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)))),
+                    onPressed: () => {},
+                    child: const Padding(
+                      padding: EdgeInsets.fromLTRB(27, 10, 27, 10),
+                      child: Text(
+                        'THOÁT',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(10)),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'images/dogde.png',
+                              fit: BoxFit.cover,
+                              width: 55,
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          'Hồ Sơ',
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'images/home.png',
+                              fit: BoxFit.cover,
+                              width: 55,
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          'Home',
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'images/swords.png',
+                              fit: BoxFit.cover,
+                              width: 50,
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          'Xếp hạng',
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              'images/store.png',
+                              fit: BoxFit.cover,
+                              width: 55,
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          'Cửa hàng',
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+          // This trailing comma makes auto-formatting nicer for build methods.
+        ),
+      ),
+    );
+  }
+}
+
+// 
