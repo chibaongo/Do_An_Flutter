496cf7395efbed6ab4433e5d7bf5aba976f9716f
PhamTanDat31052002 <phamtandat31052002@gmail.com>
2022-11-12 12:45:23 +0700

    Add giao diện nhập mã xác nhận QMK

--- flutter_application_1/lib/QMKNhapMXN.dart
+++ flutter_application_1/lib/QMKNhapMXN.dart
@@ -1,36 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/QMKNhapMXN.dart';
 
 
-class QMKNhapMail extends StatefulWidget {
-  const QMKNhapMail({super.key, required this.title});
+
+class QMKNhapMXN extends StatefulWidget {
+  const QMKNhapMXN({super.key, required this.title});
 
   final String title;
 
   @override
-  State<QMKNhapMail> createState() => _QMKNhapMail();
+  State<QMKNhapMXN> createState() => _QMKNhapMXN();
 }
 
-class _QMKNhapMail extends State<QMKNhapMail> {
+class _QMKNhapMXN extends State<QMKNhapMXN> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body:
+       Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
             image: AssetImage("assets/images/nen/nen.jpg"), fit: BoxFit.cover),
       ),
-      child:
+      child: 
       SingleChildScrollView(child: 
-       Container(
+      Container(
         padding: const EdgeInsets.all(10),
         margin: const EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
+             Padding(
                 padding: EdgeInsets.all(20.0),
                 child: Image.asset('assets/images/icon/logo1.png',width: 200,)
               ),
@@ -41,45 +42,56 @@ class _QMKNhapMail extends State<QMKNhapMail> {
                   fontWeight: FontWeight.bold,
                   color: Colors.redAccent),
             ),
-          Padding(padding: EdgeInsets.only(top: 20),child: Text("Nhập Email đã đăng ký với tài khoản ",
+          Padding(padding: EdgeInsets.only(top: 20),child: Text("Nhập mã xác nhận ",
             style: TextStyle(
               color: Colors.blue
             ),),
           ),
-            const Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 50),child: 
+            const Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 20),child: 
             const TextField(
               style: TextStyle(color: Colors.redAccent),
               decoration: InputDecoration(
-                icon: Icon(Icons.email),
+                // icon: Icon(Icons.email),
                 enabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(width: 1, color: Colors.redAccent),
                 ),
                 border: OutlineInputBorder(),
-                hintText: 'Email',
+                hintText: 'Mã xác nhận',
                 hintStyle: TextStyle(color: Colors.redAccent),
-                
               ),
             ),
             ),
-            ElevatedButton(
+            
+                 ElevatedButton(
                   style: ButtonStyle(
                       backgroundColor: MaterialStatePropertyAll<Color>(
                           const Color.fromARGB(255, 27, 247, 228)
                               .withOpacity(0.8)),
                       shape: MaterialStateProperty.all(RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30.0)))),
-                  onPressed: () => {
-                            Navigator.push(context, 
-                            MaterialPageRoute(builder: (context)=>
-                            QMKNhapMXN(title: "Nhập mã xác nhận")))
-
-                  },
+                  onPressed: () => {},
                   child: const Padding(
-                    padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                    child: Text('Lấy mã xác nhận',style: TextStyle(color: Colors.black)),
+                    padding: EdgeInsets.fromLTRB(45, 20, 45, 20),
+                    child: Text('Xác nhận',style: TextStyle(color: Colors.black)),
                   ),
             ),
-            Padding(padding: EdgeInsets.only(top: 200),child: 
+            Padding(padding: EdgeInsets.all(10)),
+             ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll<Color>(
+                          const Color.fromARGB(255, 27, 247, 228)
+                              .withOpacity(0.8)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)))),
+                  onPressed: () => {},
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(15, 20, 15, 20),
+                    child: Text('Gửi lại mã xác nhận',style: TextStyle(color: Colors.black)),
+                  ),
+            ),
+             
+
+            Padding(padding: EdgeInsets.only(top: 150),child: 
             ElevatedButton(
                   style: ButtonStyle(
                       backgroundColor: MaterialStatePropertyAll<Color>(
@@ -90,18 +102,17 @@ class _QMKNhapMail extends State<QMKNhapMail> {
                   onPressed: () => {},
                   child: const Padding(
                     padding: EdgeInsets.all(10),
-                    child: Text('Thoát',style: TextStyle(color: Colors.black)),
+                    child: Text('Quay lại',style: TextStyle(color: Colors.black)),
                   ),
-                  
             ),
-
             ),
             Padding(padding: EdgeInsets.only(bottom: 100))
           ],
         ),
       ),
-      )
     ),
+       )
     );
+
   }
 }
