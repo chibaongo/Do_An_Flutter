a460c4ad3a1ff586e47b6a75f630429d758f73bd
PhamTanDat31052002 <phamtandat31052002@gmail.com>
2022-11-12 20:58:31 +0700

    Thêm dữ liệu cửa hàng vào dbcontext

---
+++ flutter_application_1/lib/Model/dbcontext.dart
@@ -0,0 +1,24 @@
+
+class DuLieuStore{
+  const DuLieuStore({required this.image, required this.caption, required this.price});
+  final String image;
+  final String caption;
+  final int price;
+  static List<DuLieuStore> lstDuLieuStore=[
+    DuLieuStore(image: 'images/avatar/goku.png', caption: "Songoku", price: 8),
+    DuLieuStore(image: 'images/avatar/gojo.png', caption: "Gojo", price: 8),
+    DuLieuStore(image: 'images/avatar/naruto.png', caption: "Naruto", price: 10),
+  ];
+}
+class DuLieuStoreXu{
+  const DuLieuStoreXu({required this.xu, required this.price});
+  final int xu;
+  final int price;
+  static List<DuLieuStoreXu> lstDuLieuStoreXu=[
+    DuLieuStoreXu(xu: 8, price: 10000),
+    DuLieuStoreXu(xu: 20, price: 20000),
+    DuLieuStoreXu(xu: 70, price: 50000),
+    DuLieuStoreXu(xu: 150, price: 100000),
+    DuLieuStoreXu(xu: 400, price: 200000),
+  ];
+}
